7ff9d98d05880a81e03a09aeec5835812d62c756
KubaZawieruchaAllegro <jakub.zawierucha@allegro.com>
2026-07-20 21:15:30 +0200

    final Scoreboard signatures defined

--- src/main/java/pl/zawierucha/liveworldcup/domain/Scoreboard.java
+++ src/main/java/pl/zawierucha/liveworldcup/domain/Scoreboard.java
@@ -5,11 +5,11 @@ import java.util.List;
 public interface Scoreboard {
     void startMatch(MatchParticipant home, MatchParticipant visitor);
 
-    // the matches score is usually increased (or decreased) by one, for only one participant
-    // maybe it will be better to create two methods - increase score + decrease score?
-    void updateScore(MatchParticipant home, MatchParticipant visitor);
+    void increaseScore(MatchId matchId, ParticipantName participantName);
 
-    void finishMatch(MatchParticipant home, MatchParticipant visitor);
+    void decreaseScore(MatchId matchId, ParticipantName participantName);
+
+    void finishMatch(MatchId matchId);
 
     List<Match> getSummary();
 }
