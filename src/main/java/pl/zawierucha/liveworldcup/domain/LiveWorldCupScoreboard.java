873bf6ae34dfbcc2c171d40fe39e5b46acb97da6
KubaZawieruchaAllegro <jakub.zawierucha@allegro.com>
2026-07-21 19:10:38 +0200

    LiveWorldCupScoreboard tests added

--- src/main/java/pl/zawierucha/liveworldcup/domain/LiveWorldCupScoreboard.java
+++ src/main/java/pl/zawierucha/liveworldcup/domain/LiveWorldCupScoreboard.java
@@ -12,6 +12,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+// TODO handle multithreading + Copilot code review + my own functionality
 public class LiveWorldCupScoreboard implements Scoreboard {
 
     private final Map<MatchId, Match> matches = new HashMap<>();
--- src/test/java/pl/zawierucha/liveworldcup/domain/LiveWorldCupScoreboardTest.java
+++ src/test/java/pl/zawierucha/liveworldcup/domain/LiveWorldCupScoreboardTest.java
@@ -1,14 +1,23 @@
 package pl.zawierucha.liveworldcup.domain;
 
 import org.junit.jupiter.api.Test;
+import pl.zawierucha.liveworldcup.domain.exceptions.InvalidScoreException;
+import pl.zawierucha.liveworldcup.domain.exceptions.NoActiveMatchException;
+import pl.zawierucha.liveworldcup.domain.exceptions.ParticipantAlreadyInMatchException;
+import pl.zawierucha.liveworldcup.domain.exceptions.ParticipantNotFoundInMatchException;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LiveWorldCupScoreboardTest {
     private final Scoreboard scoreboard = new LiveWorldCupScoreboard();
 
     @Test
-    public void shouldAddToTheScoreboard_when_validMatchIsStarted() {
+    public void shouldAddMatchToTheScoreboard_when_startedWithValidTeams() {
         // given:
         MatchParticipant home = new MatchParticipant(new ParticipantName("Mexico"));
         MatchParticipant visitor = new MatchParticipant(new ParticipantName("Canada"));
@@ -19,6 +28,62 @@ public class LiveWorldCupScoreboardTest {
         // then:
         assertTrue(scoreboard.getSummary().stream()
                 .anyMatch(match -> match.home().equals(home) && match.visitor().equals(visitor))
+        );
+    }
+
+    @Test
+    public void shouldNotAddMatchToTheScoreboard_when_firstTeamAlreadyInMatch() {
+        // given:
+        MatchParticipant home = new MatchParticipant(new ParticipantName("Mexico"));
+        MatchParticipant visitor = new MatchParticipant(new ParticipantName("Canada"));
+        scoreboard.startMatch(home, visitor);
+
+        MatchParticipant home2 = new MatchParticipant(new ParticipantName("Mexico"));
+        MatchParticipant visitor2 = new MatchParticipant(new ParticipantName("Honduras"));
+
+        // then:
+        assertThrows(ParticipantAlreadyInMatchException.class, () -> scoreboard.startMatch(home2, visitor2));
+
+        assertTrue(scoreboard.getSummary().stream()
+                .noneMatch(match -> match.home().equals(home2) && match.visitor().equals(visitor2))
+        );
+    }
+
+    @Test
+    public void shouldNotAddMatchToTheScoreboard_when_secondTeamAlreadyInMatch() {
+        // given:
+        MatchParticipant home = new MatchParticipant(new ParticipantName("Mexico"));
+        MatchParticipant visitor = new MatchParticipant(new ParticipantName("Canada"));
+        scoreboard.startMatch(home, visitor);
+
+        MatchParticipant home2 = new MatchParticipant(new ParticipantName("Honduras"));
+        MatchParticipant visitor2 = new MatchParticipant(new ParticipantName("Canada"));
+
+        // then:
+        assertThrows(ParticipantAlreadyInMatchException.class, () -> scoreboard.startMatch(home2, visitor2));
+
+        assertTrue(scoreboard.getSummary().stream()
+                .noneMatch(match -> match.home().equals(home2) && match.visitor().equals(visitor2))
+        );
+    }
+
+    @Test
+    public void shouldAllowStartingAnotherMatch_when_noParticipantIsPlaying() {
+        // given:
+        MatchParticipant home = new MatchParticipant(new ParticipantName("Mexico"));
+        MatchParticipant visitor = new MatchParticipant(new ParticipantName("Canada"));
+        scoreboard.startMatch(home, visitor);
+
+        MatchParticipant home2 = new MatchParticipant(new ParticipantName("Spain"));
+        MatchParticipant visitor2 = new MatchParticipant(new ParticipantName("Brazil"));
+
+        // when:
+        scoreboard.startMatch(home2, visitor2);
+
+        // then:
+        assertEquals(2, scoreboard.getSummary().size());
+        assertTrue(scoreboard.getSummary().stream()
+                .anyMatch(match -> match.home().equals(home2) && match.visitor().equals(visitor2))
         );
     }
 
@@ -40,7 +105,18 @@ public class LiveWorldCupScoreboardTest {
     }
 
     @Test
-    public void shouldIncreaseScore_when_validMatchIsFound() {
+    public void shouldThrowException_when_finishingNonExistingMatch() {
+        // given:
+        MatchParticipant home = new MatchParticipant(new ParticipantName("Mexico"));
+        MatchParticipant visitor = new MatchParticipant(new ParticipantName("Canada"));
+        MatchId matchId = MatchId.fromMatchParticipants(home, visitor);
+
+        // then:
+        assertThrows(NoActiveMatchException.class, () -> scoreboard.finishMatch(matchId));
+    }
+
+    @Test
+    public void shouldIncreaseHomeScore_when_validMatchAndHomeParticipantAreFound() {
         // given:
         MatchParticipant home = new MatchParticipant(new ParticipantName("Mexico"));
         MatchParticipant visitor = new MatchParticipant(new ParticipantName("Canada"));
@@ -48,7 +124,7 @@ public class LiveWorldCupScoreboardTest {
         MatchId matchId = MatchId.fromMatchParticipants(home, visitor);
 
         // when:
-        scoreboard.increaseScore(matchId, home.getName());
+        scoreboard.increaseScore(matchId, new ParticipantName("Mexico"));
 
         // then:
         assertTrue(scoreboard.getSummary().stream()
@@ -57,7 +133,48 @@ public class LiveWorldCupScoreboardTest {
     }
 
     @Test
-    public void shouldDecreaseScore_when_validMatchIsFound() {
+    public void shouldIncreaseVisitorScore_when_validMatchAndVisitorParticipantAreFound() {
+        // given:
+        MatchParticipant home = new MatchParticipant(new ParticipantName("Mexico"));
+        MatchParticipant visitor = new MatchParticipant(new ParticipantName("Canada"));
+        scoreboard.startMatch(home, visitor);
+        MatchId matchId = MatchId.fromMatchParticipants(home, visitor);
+
+        // when:
+        scoreboard.increaseScore(matchId, new ParticipantName("Canada"));
+
+        // then:
+        assertTrue(scoreboard.getSummary().stream()
+                .anyMatch(match -> match.home().getScore() == 0 && match.visitor().getScore() == 1)
+        );
+    }
+
+    @Test
+    public void shouldThrowException_when_increasingScoreForNonExistingMatch() {
+        // given:
+        MatchParticipant home = new MatchParticipant(new ParticipantName("Mexico"));
+        MatchParticipant visitor = new MatchParticipant(new ParticipantName("Canada"));
+        MatchId matchId = MatchId.fromMatchParticipants(home, visitor);
+
+        // then:
+        assertThrows(NoActiveMatchException.class, () -> scoreboard.increaseScore(matchId, new ParticipantName("Mexico")));
+    }
+
+    @Test
+    public void shouldThrowException_when_increasingScoreForParticipantOutsideMatch() {
+        // given:
+        MatchParticipant home = new MatchParticipant(new ParticipantName("Mexico"));
+        MatchParticipant visitor = new MatchParticipant(new ParticipantName("Canada"));
+        scoreboard.startMatch(home, visitor);
+        MatchId matchId = MatchId.fromMatchParticipants(home, visitor);
+        ParticipantName participantOutsideMatch = new ParticipantName("Spain");
+
+        // then:
+        assertThrows(ParticipantNotFoundInMatchException.class, () -> scoreboard.increaseScore(matchId, participantOutsideMatch));
+    }
+
+    @Test
+    public void shouldDecreaseHomeScore_when_validMatchAndHomeParticipantAreFound() {
         // given:
         MatchParticipant home = new MatchParticipant(new ParticipantName("Mexico"));
         MatchParticipant visitor = new MatchParticipant(new ParticipantName("Canada"));
@@ -72,5 +189,118 @@ public class LiveWorldCupScoreboardTest {
         assertTrue(scoreboard.getSummary().stream()
                 .anyMatch(match -> match.home().getScore() == 0 && match.visitor().getScore() == 0)
         );
+    }
+
+    @Test
+    public void shouldThrowException_when_decreasingScoreForNonExistingMatch() {
+        // given:
+        MatchParticipant home = new MatchParticipant(new ParticipantName("Mexico"));
+        MatchParticipant visitor = new MatchParticipant(new ParticipantName("Canada"));
+        MatchId matchId = MatchId.fromMatchParticipants(home, visitor);
+
+        // then:
+        assertThrows(NoActiveMatchException.class, () -> scoreboard.decreaseScore(matchId, home.getName()));
+    }
+
+    @Test
+    public void shouldThrowException_when_decreasingScoreForParticipantOutsideMatch() {
+        // given:
+        MatchParticipant home = new MatchParticipant(new ParticipantName("Mexico"));
+        MatchParticipant visitor = new MatchParticipant(new ParticipantName("Canada"));
+        scoreboard.startMatch(home, visitor);
+        MatchId matchId = MatchId.fromMatchParticipants(home, visitor);
+        ParticipantName participantOutsideMatch = new ParticipantName("Spain");
+
+        // then:
+        assertThrows(ParticipantNotFoundInMatchException.class, () -> scoreboard.decreaseScore(matchId, participantOutsideMatch));
+    }
+
+    @Test
+    public void shouldThrowException_when_decreasingScoreBelowZero() {
+        // given:
+        MatchParticipant home = new MatchParticipant(new ParticipantName("Mexico"));
+        MatchParticipant visitor = new MatchParticipant(new ParticipantName("Canada"));
+        scoreboard.startMatch(home, visitor);
+        MatchId matchId = MatchId.fromMatchParticipants(home, visitor);
+
+        // then:
+        assertThrows(InvalidScoreException.class, () -> scoreboard.decreaseScore(matchId, home.getName()));
+    }
+
+    @Test
+    public void shouldReturnEmptyList_when_summaryRequestedForEmptyScoreboard() {
+        // then:
+        assertTrue(scoreboard.getSummary().isEmpty());
+    }
+
+    @Test
+    public void shouldSortByTotalScoreThenByStartOrder_when_summaryRequested() {
+        // given:
+        MatchParticipant home = new MatchParticipant(new ParticipantName("Mexico"));
+        MatchParticipant visitor = new MatchParticipant(new ParticipantName("Canada"));
+        scoreboard.startMatch(home, visitor);
+        MatchId matchId = MatchId.fromMatchParticipants(home, visitor);
+        for (int i = 0; i < 5; i++) {
+            scoreboard.increaseScore(matchId, visitor.getName());
+        }
+
+        home = new MatchParticipant(new ParticipantName("Spain"));
+        visitor = new MatchParticipant(new ParticipantName("Brazil"));
+        scoreboard.startMatch(home, visitor);
+        matchId = MatchId.fromMatchParticipants(home, visitor);
+        for (int i = 0; i < 10; i++) {
+            scoreboard.increaseScore(matchId, home.getName());
+        }
+        for (int i = 0; i < 2; i++) {
+            scoreboard.increaseScore(matchId, visitor.getName());
+        }
+
+        home = new MatchParticipant(new ParticipantName("Germany"));
+        visitor = new MatchParticipant(new ParticipantName("France"));
+        scoreboard.startMatch(home, visitor);
+        matchId = MatchId.fromMatchParticipants(home, visitor);
+        for (int i = 0; i < 2; i++) {
+            scoreboard.increaseScore(matchId, home.getName());
+
+            scoreboard.increaseScore(matchId, visitor.getName());
+        }
+
+        home = new MatchParticipant(new ParticipantName("Uruguay"));
+        visitor = new MatchParticipant(new ParticipantName("Italy"));
+        scoreboard.startMatch(home, visitor);
+        matchId = MatchId.fromMatchParticipants(home, visitor);
+        for (int i = 0; i < 6; i++) {
+            scoreboard.increaseScore(matchId, home.getName());
+            scoreboard.increaseScore(matchId, visitor.getName());
+        }
+
+        home = new MatchParticipant(new ParticipantName("Argentina"));
+        visitor = new MatchParticipant(new ParticipantName("Australia"));
+        scoreboard.startMatch(home, visitor);
+        matchId = MatchId.fromMatchParticipants(home, visitor);
+        for (int i = 0; i < 3; i++) {
+            scoreboard.increaseScore(matchId, home.getName());
+        }
+        for (int i = 0; i < 1; i++) {
+            scoreboard.increaseScore(matchId, visitor.getName());
+        }
+
+        List<String> expected = List.of(
+                "Uruguay-Italy:6-6:3",
+                "Spain-Brazil:10-2:1",
+                "Mexico-Canada:0-5:0",
+                "Argentina-Australia:3-1:4",
+                "Germany-France:2-2:2"
+        );
+
+        // when:
+        var summary = scoreboard.getSummary();
+
+        // then:
+        List<String> actual = summary.stream()
+                .map(m -> m.home().getName().name() + "-" + m.visitor().getName().name() + ":" + m.home().getScore() + "-" + m.visitor().getScore() + ":" + m.order())
+                .toList();
+
+        assertIterableEquals(expected, actual);
     }
 }
